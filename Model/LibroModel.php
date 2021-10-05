506ed6ff3a8e2bc9a6c4dde62a4740858275bc78
NahumLarzabal <89491971+NahumLarzabal@users.noreply.github.com>
2021-10-05 16:56:34 -0300

    search

--- Model/LibroModel.php
+++ Model/LibroModel.php
@@ -19,18 +19,6 @@ class LibroModel{
         $tarea = $sentencia->fetch(PDO::FETCH_OBJ);
         return $tarea;
     }
-    //  function getGeneros(){
-    //     $sentencia = $this->db->prepare( "select * from categorias");
-    //     $sentencia->execute();
-    //     $tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
-    //     return $tareas;
-    // }
-    // function getGenero($id){
-    //     $sentencia = $this->db->prepare( "select * from categorias WHERE id_categoria=?");
-    //     $sentencia->execute(array($id));
-    //     $tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
-    //     return $tareas;
-    // }
 
     function insertLibro($id,$autor,$nombre_libro, $descripcion, $precio, $genero){
         $sentencia = $this->db->prepare("INSERT INTO libros (id,autor, nombre_libro, descripcion, precio, id_categoria) VALUES (?,?, ?, ?, ?, ?)");
@@ -44,11 +32,7 @@ class LibroModel{
         $sentencia = $this->db->prepare("UPDATE  libros SET autor=?,nombre_libro=?,descripcion=?,precio=?,id_categoria=? WHERE libros.id = ?");
         $sentencia->execute(array($id,$autor,$nombre_libro, $descripcion, $precio, $genero));
     }
-    // function updateCategoriaFromDB($id,$categoria){
-    //     $sentencia = $this->db->prepare("UPDATE  categorias SET categoria=? WHERE categorias.id_categoria =?");
-    //     $sentencia->execute(array($id,$categoria));
-    // }
-
+  
     function insertEditLibro($id,$precio){
         $sentencia = $this->db->prepare("INSERT INTO libros (id,precio ) VALUES (?,?)");
         $sentencia->execute(array($id, $precio ));
@@ -61,13 +45,5 @@ class LibroModel{
         return $tareas;
     }
    
-    // function insertCategoria($id_categoria,$categoria){
-    //     $sentencia = $this->db->prepare("INSERT INTO categorias (id_categoria,categoria) VALUES (?,?)");
-    //     $sentencia->execute(array($id_categoria,$categoria));
-    // }
-    // function deleteCategoriaFromDB($id){
-    //     $sentencia = $this->db->prepare("DELETE FROM categorias WHERE categorias.id_categoria = ?");
-    //     $sentencia->execute(array($id));
-    // }
 
 }

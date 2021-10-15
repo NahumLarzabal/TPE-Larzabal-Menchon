931779464b46ae395401bf8df9b478b970ab098d
NahumLarzabal <89491971+NahumLarzabal@users.noreply.github.com>
2021-10-14 23:31:01 -0300

    db y user

--- db_tpe.sql
+++ db_tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-09-2021 a las 01:13:24
+-- Tiempo de generación: 15-10-2021 a las 04:29:54
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
@@ -47,7 +47,9 @@ INSERT INTO `categorias` (`id_categoria`, `categoria`) VALUES
 (8, 'Magia'),
 (9, 'Cuento'),
 (10, 'Leyenda'),
-(11, 'Fabula');
+(11, 'Fabula'),
+(12, 'Fantasmal'),
+(27, 'Heroico');
 
 -- --------------------------------------------------------
 
@@ -69,9 +71,16 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`id`, `autor`, `nombre_libro`, `descripcion`, `precio`, `id_categoria`) VALUES
-(1, 'C.S. Lewis', '\r\nLas Crónicas de Narnia: El león, la bruja y el ropero', 'Las Crónicas de Narnia (título original en inglés: The Chronicles of Narnia) es una heptalogía de libros juveniles escrita por el escritor y profesor anglo-irlandés C. S. Lewis entre 1950 y 1956, e ilustrado, en su versión original, por Pauline Baynes. Re', 3556, 7),
+(1, 'C.S. Lewis1', 'Las Crónicas de Narnia: El león, la bruja y el ropero1', ' Las Crónicas de Narnia (título original en inglés: The Chronicles of Narnia) es una heptalogía de libros juveniles escrita por el escritor y profesor anglo-irlandés C. S. Lewis entre 1950 y 1956, e ilustrado, en su versión original, por Pauline Baynes. Re1', 35562, 3),
 (2, 'C.S. Lewis', '\r\nLas crónicas de Narnia: el príncipe Caspian ', 'Las Crónicas de Narnia: El príncipe Caspian (título original en inglés: The Chronicles of Narnia: Prince Caspian) es una película de acción y fantasía dirigida por Andrew Adamson y basada en la novela homónima de C. S. Lewis. Producida por Walden Media y distribuida por Walt Disney Pictures, se estrenó el 16 de mayo de 2008 en Estados Unidos. Es la segunda entrega de la saga Las Crónicas de Narnia.', 4566, 7),
-(4, 'José Mauro de Vasconcelos', 'Mi planta de naranja lima', 'Mi planta de naranja lima (en portugués O Meu Pé de Laranja Lima) es una novela de José Mauro de Vasconcelos, una de las más leídas de la nueva literatura brasileña. El autor continúa la historia en Vamos a calentar el sol. Está narrada en primera persona y posee un altísimo nivel autobiográfico.\r\nEncabezó la lista de superventas en 1968, año de su primera edición. Posteriormente, la novela fue traducida a 32 idiomas y publicada en 19 países. Ha sido adoptado como texto lectura a nivel de enseñanza primaria.1​\r\nTres telenovelas se han realizado sobre la base de esta obra: en 1970 para la Rede Tupi, y en 1980 y 1998 para la Rede Bandeirantes. También se han realizado varias adaptaciones al cine, televisión y teatro, siendo dirigida por Aurelio Teixeira la primera para el cine en 1970.2​ En el 2011 se ha presentado su segunda versión cinematográfica.3​', 1178, 1);
+(4, 'José Mauro de Vasconcelo', 'Mi planta de naranja lima', 'Mi planta de naranja lima (en portugués O Meu Pé de Laranja Lima) es una novela de José Mauro de Vasconcelos, una de las más leídas de la nueva literatura brasileña. El autor continúa la historia en Vamos a calentar el sol. Está narrada en primera persona y posee un altísimo nivel autobiográfico.\r\nEncabezó la lista de superventas en 1968, año de su primera edición. Posteriormente, la novela fue traducida a 32 idiomas y publicada en 19 países. Ha sido adoptado como texto lectura a nivel de enseñanza primaria.1​\r\nTres telenovelas se han realizado sobre la base de esta obra: en 1970 para la Rede Tupi, y en 1980 y 1998 para la Rede Bandeirantes. También se han realizado varias adaptaciones al cine, televisión y teatro, siendo dirigida por Aurelio Teixeira la primera para el cine en 1970.2​ En el 2011 se ha presentado su segunda versión cinematográfica.3​   ', 11781, 5),
+(31, 'garcia', 'asdasd11', '363edfgdfg', 222, 6),
+(32, 'garcia', 'sadda', '1', 2, 3),
+(33, 'asdasd1231', 'asdasdasdsa', '657641', 11, 1),
+(34, 'asdasd1231', 'asdasdasdsa', '657641', 11, 1),
+(35, 'asdasd1231', 'asdasdasdsa', '657641', 11, 1),
+(36, 'asdasd1231', 'asdasdasdsa', '657641', 11, 1),
+(37, 'garcia', 'azul mar', 'adasd', 1231, 3);
 
 --
 -- Índices para tablas volcadas
@@ -98,13 +107,13 @@ ALTER TABLE `libros`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Restricciones para tablas volcadas
--- db_usertp.sql
+++ db_usertp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-10-2021 a las 23:17:41
+-- Tiempo de generación: 15-10-2021 a las 04:29:41
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.10
 
@@ -29,15 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
-  `password` varchar(150) NOT NULL
+  `nombre_apellido` varchar(200) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `tipoUser` int(11) DEFAULT 2
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`email`, `password`) VALUES
-('admin@gmail.com', '$2y$10$EMtVb9dKQ0E5WT7/zfm9huUHTx9ZOUe2JY5dvPZiLAKdXePlw2DYa');
+INSERT INTO `users` (`email`, `nombre_apellido`, `password`, `tipoUser`) VALUES
+('admin2@gmail.com', 'admin', '$2y$10$zxeMjYHmuEwiiO5aNGBkqeGewsKGEG1IadCjUxOgq9gkE05qBlQRm', 2),
+('admin@gmail.com', 'Admin', '$2y$10$EMtVb9dKQ0E5WT7/zfm9huUHTx9ZOUe2JY5dvPZiLAKdXePlw2DYa', 1);
 
 --
 -- Índices para tablas volcadas
